e6b08b48af472e4035b0ebbe6344a4ecc81f1a5c
Lormond <jangru@mail.ru>
2024-03-07 18:49:48 +0300

    added print properties

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
 public class Main {
-private int n = 5;
-private int m = 5;
+private static int n = 5;
+private static int m = 5;
 
   public static void main(String[] args) {
     System.out.println("Hello GIT");
+    fillMatrix(n, m);
   }
 
-  public void fillMatrix(int n, int m) {
-    int array[][] = new int[n][m];
+  public static void fillMatrix(int n, int m) {
+    String array[][] = new String[n][m];
     for(int i = 0; i < n; i++) {
       for(int j = 0; j < m; j++) {
-        array[n][m] = 0;
-        System.out.println(array[n][m]);
+        array[i][j] = "+";
+        System.out.println(array[i][j]);
       }
       System.out.println("\n");
     }
